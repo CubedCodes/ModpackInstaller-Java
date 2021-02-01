a3b53b8219802ddd04edcc498199d46e968ebc73
Cubed <61441648+CubedCodes@users.noreply.github.com>
2021-02-01 15:36:54 -0800

    bruh

---
+++ ModpackInstaller/src/main/java/com/rishabhroy/ModpackInstaller/NewMain.java
@@ -0,0 +1,7 @@
+package com.rishabhroy.ModpackInstaller;
+
+public class NewMain {
+	public static void main(String[] args) {
+		App.main(args);
+	}
+}
---
+++ ModpackInstaller/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.rishabhroy.ModpackInstaller {
+    requires javafx.controls;
+    exports com.rishabhroy.ModpackInstaller;
+}
