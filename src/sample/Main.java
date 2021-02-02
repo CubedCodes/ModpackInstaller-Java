85f6d49fbcb8c2b462b65054df7ed5f4904e2a0d
Blake Haug <bnh440@icloud.com>
2021-02-01 17:34:54 -0800

    New Project

---
+++ src/sample/Main.java
@@ -0,0 +1,42 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Hello World");
+        VBox pane = new VBox();
+        pane.setPrefSize(500,200);
+        pane.setSpacing(10);
+
+        Button button = new Button("Install");
+        pane.getChildren().add(button);
+
+        Text welcomeText = new Text("Welcome to the modpack installer!");
+        pane.getChildren().add(welcomeText);
+
+        VBox.setMargin(button, new Insets(20, 20, 20, 20));
+        VBox.setMargin(welcomeText, new Insets(20, 20, 20, 20));
+
+        primaryStage.setScene(new Scene(pane));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
